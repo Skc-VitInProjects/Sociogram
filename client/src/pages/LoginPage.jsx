e4068c5ce386972ada37ae3b470a29b29b4a5f83
Skand-VitInProjects <iskc9555@gmail.com>
2025-05-12 14:44:58 +0530

    Completed Profile Page

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -3,14 +3,14 @@ import assets from '../assets/assets';
 
 const LoginPage = () => {
 
-  const [currState , setCurrState] = useState("Sign Up");
+  const [currState , setCurrState] = useState("Sign Up");     //current state is set to "Sign Up"
   const [fullName , setFullName] = useState("");
   const [email , setEmail] = useState("");
   const [password , setPassword] = useState("");
   const [bio , setBio] =useState("");
-  const [isDataSubmitted , setIsDataSubmitted] = useState(false);
+  const [isDataSubmitted , setIsDataSubmitted] = useState(false);    //by default , the data is unsubmitted , i.e. false
 
-  const onSubmitHandler = (event) => {
+  const onSubmitHandler = (event) => {                 //function to handle user submition of details
      event.preventDefault();
 
      if(currState === "Sign Up" && !isDataSubmitted){
@@ -23,32 +23,35 @@ const LoginPage = () => {
     <div className='min-h-screen bg-cover bg-center flex items-center 
        justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl '>
          
-         {/* --------------left------------ */}
+         {/* --------------left---------logo is added--- */}
          <div>
             <img src={assets.logo_big} alt="" className='w-[min(30vw , 250px)] h-50'/>
             <h1 className='text-white px-10 text-2xl' >Sociogram</h1>
          </div>
 
-         {/* -----------right-------------- */}
+         {/* -----------right----------form Details---- */}
          <form onSubmit = {onSubmitHandler}
            className='border-2 bg-[#212121] text-white border-orange-500 p-6 flex 
              flex-col gap-6 rounded-lg shadow-lg'>
               <h2 className='font-medium text-2xl flex justify-between items-center'>
-                  {currState}
+                {/* CurrState is Sign Up */}
+                  {currState}     
                    {isDataSubmitted && <img onClick={() => setIsDataSubmitted(false)} src={assets.arrow_icon} 
                       alt="" className='w-5 cursor-pointer' />
                    }
               </h2>
               
-              {currState === "Sign Up" && !isDataSubmitted && (
+              {/* Full name  */}
+              {currState === "Sign Up" && !isDataSubmitted && (                //data is unsubmitted
                     <input onChange={(e) => setFullName(e.target.value)} value= {fullName}
-                     type="text" className='p-2 border border-yellow-500 rounded-md 
-                      focus:outline-none focus:ring-2 focus:ring-yellow-500' 
-                      placeholder= "Full Name" required />   
+                       type="text" className='p-2 border border-yellow-500 rounded-md 
+                        focus:outline-none focus:ring-2 focus:ring-yellow-500' 
+                       placeholder= "Full Name" required />   
                     
               )}
               
-              {!isDataSubmitted && (
+              {/* once , name is entered , other details can be entered */}
+              {!isDataSubmitted && (                   //data is unsubmitted
                  <>
                   <input onChange={(e) => setEmail(e.target.value)}  value = {email}
                     type="email" placeholder='Email Address' required 
@@ -61,7 +64,8 @@ const LoginPage = () => {
                       focus:ring-yellow-500'/>
                  </>
               )}
-
+                
+                {/* when data is submitted then , fill bio */}
               {
                 currState === "Sign Up" && isDataSubmitted && (
                    <textarea  onChange={(e) => setBio(e.target.value)} value ={bio}
@@ -72,9 +76,12 @@ const LoginPage = () => {
                 )
               }
 
+              {/* Submit details */}
               <button type="submit" className='py-3 bg-gradient-to-r from-yellow-400 to-orange-600
                   text-white rounded-md cursor-pointer'>
-                    {currState === "Sign Up" ? "Create Account" : "Login Now"}
+                    
+                    {/* if state is Sign Up , then user will create account , else he will login */}
+                    {currState === "Sign Up" ? "Create Account" : "Login Now"}         
               </button>
 
               <div className='flex items-center gap-2 text-sm text-gray-500'>
@@ -85,11 +92,11 @@ const LoginPage = () => {
               <div className='flex flex-col gap-2'>
                 {currState === "Sign Up" ? (
                   <p className='text-sm text-gray-600'>Already have an account ?
-                  <span onClick={() => {setCurrState("Login"); setIsDataSubmitted(false)}} 
+                  <span onClick={() => {setCurrState("Login"); setIsDataSubmitted(false)}}           //changes the state from Sign Up to Login
                     className='font-medium text-yellow-500 cursor-pointer'>&nbsp;&nbsp;Login here</span></p>
                 ) : (
                   <p className='text-sm text-gray-600'>Create an account 
-                  <span onClick={() => setCurrState("Sign Up")} 
+                  <span onClick={() => setCurrState("Sign Up")}            
                     className='font-medium text-yellow-500 cursor-pointer'>&nbsp;&nbsp;Click here</span></p>
                 )}
               </div>
--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,9 +1,63 @@
-import React from 'react';
+import React , {useState}from 'react';
+import { useNavigate } from 'react-router-dom';
+import assets from '../assets/assets';
 
 const ProfilePage = () => {
+
+  const [selectedImg , setSelectedImg] = useState(null);
+  const navigate = useNavigate();
+
+  const [name , setName] = useState("Martin Johnson");       //dummy name given 
+  const [bio , setBio] = useState("Hi Everyone, I am Using Sociogram");
+ 
+  // function to handle the page after clicking on save button
+  const handleSubmit = async (e)=> {
+     e.preventDefault();
+     navigate('/');            //takes to HomePage to Chat Window
+  }
+
   return (
-    <div>
-      <h1>Profile Page</h1>
+    <div className='min-h-screen bg-cover bg-no-repeat flex items-center justify-center'>
+
+        <div className='w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-orange-600 
+          flex items-center justify-between max-sm:flex-col-reverse rounded-lg'>
+
+          {/* Form Details */}
+          <form onSubmit = {handleSubmit}
+             className='flex flex-col gap-5 p-10 flex-1'>
+            <h3 className='text-lg '>Profile Details</h3>
+            <label htmlFor='avatar' className='flex items-center gap-3 cursor-pointer'> 
+                
+               <input onChange = {(e) => setSelectedImg(e.target.files[0])} type = "file"  //user uploads image here
+                 id = "avatar" accept='.png , .jpg , .jpeg' hidden />
+
+               <img src ={selectedImg ? URL.createObjectURL(selectedImg) : assets.avatar_icon} alt="" 
+                 className= {`w-12 h-12 ${selectedImg && 'rounded-full'}`} />
+                  upload profile image
+            </label>
+
+            <input onChange={(e) => setName(e.target.value)} value={name}         //entering name here
+              type="text" required placeholder ="Your Name" 
+              className='p-2 border border-yellow-500 rounded-md 
+                focus:outline-none focus:ring-2 focus:ring-yellow-500' />
+
+            <textarea onChange={(e) => setBio(e.target.value)} value = {bio}     //entering bio here
+               placeholder='Write profile bio' required 
+               className='p-2 border border-yellow-500 rounded-md 
+                 focus:outline-none focus:ring-2 focus:ring-yellow-500' rows={4}>
+            </textarea>
+            
+            {/* Finally , submitting and saving the profile details */}
+            <button type="submit" className='bg-gradient-to-r from-yellow-400 to-orange-600 text-white p-2 rounded-full text-lg cursor-pointer'>
+                 Save
+            </button>
+               
+          </form> 
+            
+          {/* Logo on the right side */}
+          <img className="max-w-40 aspect-square rounded-full mx-10 max-sm:mt-10" 
+            src={assets.logo_icon} alt="" />
+        </div>
     </div>
   );
 }
